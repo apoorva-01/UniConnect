611232256cddef471a44740a996288430ecd99e1
Apoorva Verma <76636952+apoorva-01@users.noreply.github.com>
2023-10-28 18:59:22 +0530

    interests developed

---
+++ components/ui/Interest.jsx
@@ -0,0 +1,45 @@
+import * as React from 'react';
+import AspectRatio from '@mui/joy/AspectRatio';
+import Link from '@mui/joy/Link';
+import Card from '@mui/joy/Card';
+import CardContent from '@mui/joy/CardContent';
+import Typography from '@mui/joy/Typography';
+
+export default function Interest(props) {
+  return (
+    <Card
+      variant="outlined"
+      orientation="horizontal"
+      sx={{
+        width: 320,
+        margin:"1rem",
+        '&:hover': { boxShadow: 'md', borderColor: 'neutral.outlinedHoverBorder' },
+      }}
+    >
+      <AspectRatio ratio="1" sx={{ width: 90 }}>
+        <img
+          src="https://images.unsplash.com/photo-1507833423370-a126b89d394b?auto=format&fit=crop&w=90"
+          srcSet="https://images.unsplash.com/photo-1507833423370-a126b89d394b?auto=format&fit=crop&w=90&dpr=2 2x"
+          loading="lazy"
+          alt=""
+        />
+      </AspectRatio>
+      <CardContent>
+        <Typography level="title-lg" id="card-description">
+        {props.name}
+        </Typography>
+        <Typography level="body-sm" aria-describedby="card-description" mb={1}>
+          <Link
+            overlay
+            underline="none"
+            href="#interactive-card"
+            sx={{ color: 'text.tertiary' }}
+          >
+            {props.desc}
+          </Link>
+        </Typography>
+       
+      </CardContent>
+    </Card>
+  );
+}
